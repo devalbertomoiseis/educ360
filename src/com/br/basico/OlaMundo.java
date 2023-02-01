10a59731fd6ce602b1c11284438b53327fb2a7e4
albertomoiseisdev <contatoalbertomoiseis@gmail.com>
2023-02-01 16:03:46 -0300

    Trabalhando com Variaveis

--- src/com/br/basico/OlaMundo.java
+++ src/com/br/basico/OlaMundo.java
@@ -7,5 +7,20 @@ public class OlaMundo {
         // Quebra de Linha \n
         System.out.println("Java !!!");
         System.out.println("Olá Mundo !!!");
+
+
+        byte numeroInteiroMuitoPequen = 127;
+        short numeroInteiroPequeno = 12345;
+        int numeroInteiro = 123456789;
+        long numeroInteiroGrande = 123456789123456L;
+        float numeroReal = 12345678F;
+        double numeroRealGrade = 123456789;
+        System.out.println("byte : " + numeroInteiroMuitoPequen);
+        System.out.println("short : " + numeroInteiroPequeno);
+        System.out.println("int : " + numeroInteiro);
+        System.out.println("long : " + numeroInteiroGrande);
+        System.out.println("float : " + numeroReal);
+        System.out.println("double : " + numeroRealGrade);
+        System.out.println("Hello world!");
     }
 }
