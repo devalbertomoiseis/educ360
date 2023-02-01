d3b483d91dd70dd663977e10b32ec6cdcff799b4
albertomoiseisdev <contatoalbertomoiseis@gmail.com>
2023-02-01 15:58:06 -0300

    Trabalhando com variaveis - Java

--- src/com/br/basico/OlaMundo.java
+++ src/com/br/basico/OlaMundo.java
@@ -4,6 +4,7 @@ public class OlaMundo {
     public static void main(String[] args) {
         // 1 - Comentário
         /* 2 - Comentário*/
+        // Quebra de Linha \n
         System.out.println("Java !!!");
         System.out.println("Olá Mundo !!!");
     }
