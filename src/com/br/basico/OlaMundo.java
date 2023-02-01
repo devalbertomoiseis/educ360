a0b0a6bb4dc2fce993f90804352d3daa580d5f33
albertomoiseisdev <contatoalbertomoiseis@gmail.com>
2023-02-01 15:27:31 -0300

    Trabalhando com comentários - Java

--- src/com/br/basico/OlaMundo.java
+++ src/com/br/basico/OlaMundo.java
@@ -2,6 +2,9 @@ package com.br.basico;
 
 public class OlaMundo {
     public static void main(String[] args) {
+        // 1 - Comentário
+        /* 2 - Comentário*/
+        System.out.println("Java !!!");
         System.out.println("Olá Mundo !!!");
     }
 }
